b12f87785a76894e8d29d11f11d2aec44517d82f
mhdalhzau <mhdalhzau@gmail.com>
2025-08-28 20:40:44 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -529,11 +529,6 @@ const disableIosTextFieldZoom = addMaximumScaleToMetaViewport;
 const checkIsIOS = () =>
   /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-const badge = document.querySelector('.badge'); 
-  badge.classList.add('hidden');   
-  badge.classList.remove('hidden');
-  badge.classList.toggle('hidden'); 
-
 if (checkIsIOS()) {
   disableIosTextFieldZoom();
 }
