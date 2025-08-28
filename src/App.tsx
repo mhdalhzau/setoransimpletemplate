78c319e207e3ac2bd8c0d193f81651f9c3acebf2
mhdalhzau <mhdalhzau@gmail.com>
2025-08-28 20:54:16 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -417,11 +417,11 @@ Total Pemasukan: Rp ${formatRupiah(totalPemasukan)}
             >
               <Copy size={16} />
               Copy ke Clipboard
-            </button><br>
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </div><hr>
   );
 }
 
